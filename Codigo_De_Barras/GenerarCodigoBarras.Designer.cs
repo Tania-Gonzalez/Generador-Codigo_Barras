680c048575bebc1560fdf0ae612d0c9344c0164a
Tania-Gonzalez <gonz15@gmail.com>
2020-06-09 02:30:59 -0600

    Bloque 1

---
+++ Codigo_De_Barras/GenerarCodigoBarras.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Codigo_De_Barras
+{
+    partial class GenerarCodigoBarras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.PanelResultado = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Texto";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(27, 170);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(400, 20);
+            this.txtTexto.TabIndex = 10;
+            // 
+            // PanelResultado
+            // 
+            this.PanelResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelResultado.Location = new System.Drawing.Point(27, 36);
+            this.PanelResultado.Name = "PanelResultado";
+            this.PanelResultado.Size = new System.Drawing.Size(400, 100);
+            this.PanelResultado.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Código de Barras";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Location = new System.Drawing.Point(186, 206);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 25);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click_1);
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(79, 206);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 25);
+            this.btnGenerar.TabIndex = 6;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Enabled = false;
+            this.btnImprimir.Location = new System.Drawing.Point(290, 206);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 25);
+            this.btnImprimir.TabIndex = 12;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // GenerarCodigoBarras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 254);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.PanelResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "GenerarCodigoBarras";
+            this.Text = "GenerarCodigoBarras";
+            this.Load += new System.EventHandler(this.GenerarCodigoBarras_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Panel PanelResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
---
+++ Codigo_De_Barras/GenerarCodigoBarras.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using BarcodeLib;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Codigo_De_Barras
+{
+    public partial class GenerarCodigoBarras : Form
+    {
+  
+
+        public GenerarCodigoBarras()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerarCodigoBarras_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            // Importar la Librería BarcodeLib en NuGet
+
+            BarcodeLib.Barcode CodigoBarras = new BarcodeLib.Barcode();
+            CodigoBarras.IncludeLabel = true;
+            PanelResultado.BackgroundImage = CodigoBarras.Encode(BarcodeLib.TYPE.CODE128, txtTexto.Text, Color.Black, Color.White, 400, 100); // <---------
+            btnGuardar.Enabled = true;
+            btnImprimir.Enabled = true;
+
+        }
+
+        private void btnGuardar_Click_1(object sender, EventArgs e)
+        {
+            Image imgFinal = (Image)PanelResultado.BackgroundImage.Clone();
+
+            SaveFileDialog CajaDeDiaologoGuardar = new SaveFileDialog();
+            CajaDeDiaologoGuardar.AddExtension = true;
+            CajaDeDiaologoGuardar.Filter = "Image JPEG (*.jpeg)|*.jpeg";
+            CajaDeDiaologoGuardar.ShowDialog();
+            if (!string.IsNullOrEmpty(CajaDeDiaologoGuardar.FileName))
+            {
+                imgFinal.Save(CajaDeDiaologoGuardar.FileName, ImageFormat.Jpeg);
+            }
+            imgFinal.Dispose();
+
+        }
+
+
+
+        private void Metodo_Imprimir(object sender, PrintPageEventArgs e)
+        {
+
+            Image imgFinal = (Image)PanelResultado.BackgroundImage.Clone();
+
+   
+            e.Graphics.DrawImage(imgFinal, new Rectangle(0, 100, 400, 100)); //<-----
+
+
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+
+            printDocument1 = new System.Drawing.Printing.PrintDocument();
+            PrinterSettings ps = new PrinterSettings();
+
+            printDocument1.PrinterSettings = ps;
+            printDocument1.PrintPage += Metodo_Imprimir;
+            printDocument1.Print();
+
+
+        }
+
+
+
+        
+
+        
+    }
+}
